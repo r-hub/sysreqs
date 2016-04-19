f8a55d6c210191619feb564978038d13a5cc5d61
Gabor Csardi <csardi.gabor@gmail.com>
2016-04-19 15:33:49 +0100

    Need to encode / characters in sysreqs field

--- R/sysreqs.R
+++ R/sysreqs.R
@@ -52,7 +52,7 @@ get_sysreqs <- function(query, platform = current_platform()) {
   query <- as.character(query)[1]
   url <- make_url(
     sysreqs_url,
-    query = URLencode(query),
+    query = encode_slash(URLencode(query)),
     platform = platform
   )
 
--- R/utils.R
+++ R/utils.R
@@ -4,3 +4,7 @@ os_type <- function() {
 }
 
 `%||%` <- function(l, r) { if (is.null(l)) r else l }
+
+encode_slash <- function(x) {
+  gsub("/", "&#47;", x, fixed = TRUE)
+}
